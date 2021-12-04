eb5b316405b198839e25d90c8a997006add05a5d
alec <anhvouw@gmail.com>
2021-12-04 08:53:25 -0500

    day 2 pt 2

--- 2021/day2/puzzle.go
+++ 2021/day2/puzzle.go
@@ -36,3 +36,34 @@ func Dive(fileName string) int {
 	}
 	return x * y
 }
+
+func Dive2(fileName string) int {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	x, y, aim := 0, 0, 0
+	for scanner.Scan() {
+		t := scanner.Text()
+		ins := strings.Split(t, " ")
+		units, err := strconv.Atoi(ins[1])
+		if err != nil {
+			return 0
+		}
+
+		switch ins[0] {
+		case "forward":
+			x += units
+			y += (aim * units)
+		case "up":
+			aim -= units
+		case "down":
+			aim += units
+		}
+	}
+	return x * y
+}
--- 2021/day2/puzzle_test.go
+++ 2021/day2/puzzle_test.go
@@ -9,3 +9,11 @@ func TestPuzzle(t *testing.T) {
 		t.Fatalf("Expected 5 but got %d", n)
 	}
 }
+
+func TestPuzzle2(t *testing.T) {
+	n := Dive2("input.txt")
+
+	if n != 1842742223 {
+		t.Fatalf("Expected 1842742223 but got %d", n)
+	}
+}
